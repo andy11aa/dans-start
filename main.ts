7e6d3db8a7214910eae77accb34c10d2ca680fda
andy11aa <125970868+andy11aa@users.noreply.github.com>
2023-03-03 02:04:59 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,3 @@
-function ansikt_blunk () {
-    led.plot(1, 1)
-    led.plot(3, 1)
-    led.plot(0, 3)
-    led.plot(1, 4)
-    led.plot(2, 4)
-    led.plot(3, 4)
-    led.plot(4, 3)
-    led.plot(0, 1)
-    led.plot(4, 1)
-    led.plot(1, 0)
-    led.plot(0, 0)
-}
-function ansikt_glad () {
-    led.plot(1, 1)
-    led.plot(3, 1)
-    led.plot(0, 3)
-    led.plot(1, 4)
-    led.plot(2, 4)
-    led.plot(3, 4)
-    led.plot(4, 3)
-    led.plot(0, 1)
-    led.plot(1, 0)
-    led.plot(0, 0)
-    led.plot(4, 1)
-    led.plot(4, 0)
-    led.plot(3, 0)
-}
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 0) {
         starta = true
@@ -35,19 +7,77 @@ radio.onReceivedNumber(function (receivedNumber) {
         bitbot.stop(BBStopMode.Coast)
     }
 })
-function føl_linjen () {
-    if (bitbot.readLine(BBLineSensor.Left) == 0 && bitbot.readLine(BBLineSensor.Right) == 1) {
-        if (bitbot.readLine(BBLineSensor.Left) == 0 && bitbot.readLine(BBLineSensor.Right) == 1) {
-            bitbot.rotate(BBRobotDirection.Left, 20)
-        } else if (bitbot.readLine(BBLineSensor.Right) == 1 && bitbot.readLine(BBLineSensor.Left) == 0) {
-            bitbot.rotate(BBRobotDirection.Right, 20)
-        } else {
-            bitbot.go(BBDirection.Forward, 20)
-        }
-    } else if (bitbot.readLine(BBLineSensor.Right) == 0 && bitbot.readLine(BBLineSensor.Left) == 0) {
-        bitbot.stop(BBStopMode.Coast)
-    } else {
-        bitbot.go(BBDirection.Forward, 20)
+function alle_smilefes (tekst: string) {
+    if (tekst == "glad") {
+        led.plot(1, 1)
+        led.plot(3, 1)
+        led.plot(0, 3)
+        led.plot(1, 4)
+        led.plot(2, 4)
+        led.plot(3, 4)
+        led.plot(4, 3)
+        led.plot(0, 1)
+        led.plot(1, 0)
+        led.plot(0, 0)
+        led.plot(4, 1)
+        led.plot(4, 0)
+        led.plot(3, 0)
+    } else if (tekst == "blunk") {
+        led.plot(1, 1)
+        led.plot(3, 1)
+        led.plot(0, 3)
+        led.plot(1, 4)
+        led.plot(2, 4)
+        led.plot(3, 4)
+        led.plot(4, 3)
+        led.plot(0, 1)
+        led.plot(4, 1)
+        led.plot(1, 0)
+        led.plot(0, 0)
+    } else if (tekst == "ansikt å") {
+        led.plot(1, 1)
+        led.plot(3, 1)
+        led.plot(0, 3)
+        led.plot(1, 4)
+        led.plot(2, 4)
+        led.plot(3, 4)
+        led.plot(4, 3)
+        led.plot(0, 1)
+        led.plot(1, 0)
+        led.plot(0, 0)
+        led.plot(4, 1)
+        led.plot(4, 0)
+        led.plot(3, 0)
+        led.plot(1, 2)
+        led.plot(2, 2)
+        led.plot(3, 2)
+    } else if (tekst == "ansikt o") {
+        led.plot(1, 1)
+        led.plot(3, 1)
+        led.plot(3, 0)
+        led.plot(2, 4)
+        led.plot(0, 1)
+        led.plot(1, 0)
+        led.plot(0, 0)
+        led.plot(4, 1)
+        led.plot(4, 0)
+        led.plot(2, 2)
+        led.plot(1, 3)
+        led.plot(3, 3)
+    } else if (tekst == "lei") {
+        led.plot(1, 1)
+        led.plot(3, 1)
+        led.plot(0, 4)
+        led.plot(1, 3)
+        led.plot(2, 3)
+        led.plot(3, 3)
+        led.plot(4, 4)
+        led.plot(0, 1)
+        led.plot(1, 0)
+        led.plot(0, 0)
+        led.plot(4, 1)
+        led.plot(4, 0)
+        led.plot(3, 0)
     }
 }
 function vent () {
@@ -57,13 +87,16 @@ function vent () {
     }
 }
 function r5 () {
-    oåoåoå()
+    oåoåoå_finetå(R5)
     led_e_blinke()
-    servo()
-    tibake_og_fram()
-    dancmove_1()
-    dancmove_3()
-    dancmove_4()
+    basic.pause(BPM * 2)
+    startlys = true
+    bitbot.goms(BBDirection.Forward, 60, BPM2 * 5)
+    basic.pause(BPM2 * 5)
+    basic.pause(BPM * 3)
+    while (bitbot.readLine(BBLineSensor.Left) == 0 || bitbot.readLine(BBLineSensor.Right) == 0) {
+        bitbot.go(BBDirection.Forward, 20)
+    }
 }
 function _1 () {
     bitbot.setLedColor(0xFF0080)
@@ -84,19 +117,16 @@ function oåoåoå () {
     for (let index = 0; index < 2; index++) {
         for (let index = 0; index < 3; index++) {
             pins.servoWritePin(AnalogPin.P2, 180)
-            ansikt_o()
             pins.servoWritePin(AnalogPin.P1, 0)
             basic.pause(BPM / 2)
             basic.clearScreen()
             basic.pause(1)
             pins.servoWritePin(AnalogPin.P2, 90)
-            ansikt_å()
             pins.servoWritePin(AnalogPin.P1, 90)
             basic.pause(BPM / 2)
             basic.clearScreen()
             basic.pause(1)
         }
-        ansikt_å()
         basic.pause(BPM)
         basic.clearScreen()
     }
@@ -141,15 +171,68 @@ function _2 () {
     bitbot.setLedColor(0xFF0080)
 }
 function r4 () {
-    oåoåoå()
+    oåoåoå_finetå(R4)
     led_e_blinke()
-    servo()
-    tibake_og_fram()
-    dancmove_1()
-    dancmove_3()
-    dancmove_4()
+    basic.pause(BPM * 2)
+    startlys = true
+    bitbot.goms(BBDirection.Forward, 60, BPM2 * 5)
+    basic.pause(BPM2 * 5)
+    basic.pause(BPM * 3)
+    while (bitbot.readLine(BBLineSensor.Left) == 0 || bitbot.readLine(BBLineSensor.Right) == 0) {
+        bitbot.go(BBDirection.Forward, 20)
+    }
+}
+function oåoåoå_finetå (tall: number) {
+    if (tall == R1) {
+        basic.pause(BPM * 1.5)
+        for (let index = 0; index < 3; index++) {
+            pins.servoWritePin(AnalogPin.P2, 180)
+            alle_smilefes("ansikt o")
+            pins.servoWritePin(AnalogPin.P1, 0)
+            basic.pause(BPM / 2)
+            basic.clearScreen()
+            basic.pause(1)
+            pins.servoWritePin(AnalogPin.P2, 90)
+            alle_smilefes("ansikt å")
+            pins.servoWritePin(AnalogPin.P1, 90)
+            basic.pause(BPM / 2)
+            basic.clearScreen()
+            basic.pause(1)
+        }
+        alle_smilefes("ansikt å")
+        basic.pause(BPM)
+        basic.clearScreen()
+    } else {
+        basic.pause(BPM * 5)
+    }
+    if (tall == R2 || tall == R3) {
+        basic.pause(BPM * 1.5)
+        for (let index = 0; index < 3; index++) {
+            pins.servoWritePin(AnalogPin.P2, 180)
+            alle_smilefes("ansikt o")
+            pins.servoWritePin(AnalogPin.P1, 0)
+            basic.pause(BPM / 2)
+            basic.clearScreen()
+            basic.pause(1)
+            pins.servoWritePin(AnalogPin.P2, 90)
+            alle_smilefes("ansikt å")
+            pins.servoWritePin(AnalogPin.P1, 90)
+            basic.pause(BPM / 2)
+            basic.clearScreen()
+            basic.pause(1)
+        }
+        alle_smilefes("ansikt å")
+        basic.pause(BPM)
+        basic.clearScreen()
+        basic.pause(BPM * 2)
+    } else {
+        basic.pause(BPM * 7)
+    }
+    vent()
+    basic.pause(BPM)
 }
 input.onButtonPressed(Button.A, function () {
+    bitbot.go(BBDirection.Forward, 100)
     basic.showNumber(control.deviceSerialNumber())
 })
 function _4 () {
@@ -163,30 +246,19 @@ function _4 () {
     basic.pause(BPM)
     bitbot.ledClear()
 }
-function ansikt_å () {
-    led.plot(1, 1)
-    led.plot(3, 1)
-    led.plot(0, 3)
-    led.plot(1, 4)
-    led.plot(2, 4)
-    led.plot(3, 4)
-    led.plot(4, 3)
-    led.plot(0, 1)
-    led.plot(1, 0)
-    led.plot(0, 0)
-    led.plot(4, 1)
-    led.plot(4, 0)
-    led.plot(3, 0)
-    led.plot(1, 2)
-    led.plot(2, 2)
-    led.plot(3, 2)
-}
 function tibake_og_fram () {
     bitbot.goms(BBDirection.Forward, 1, 1)
     bitbot.goms(BBDirection.Reverse, 60, 400)
     vent()
     bitbot.goms(BBDirection.Forward, 60, 400)
     vent()
+}
+function gjørNoe (tekst: string) {
+    if (tekst == "glad") {
+    	
+    } else if (false) {
+    	
+    }
 }
 function kjøre_plan () {
     if (control.deviceSerialNumber() == R1) {
@@ -204,51 +276,72 @@ function kjøre_plan () {
 function led_e_blinke () {
     for (let index = 0; index < 2; index++) {
         led_e()
+        bitbot.setLedColor(0xFF0000)
         basic.pause(BPM / 2)
         basic.clearScreen()
+        bitbot.ledClear()
         basic.pause(BPM / 2)
     }
     basic.clearScreen()
     led_1()
+    bitbot.setLedColor(0xFF0000)
     basic.pause(BPM)
     basic.clearScreen()
+    bitbot.ledClear()
     basic.pause(BPM)
-    ansikt_glad()
+    alle_smilefes("glad")
 }
 function r1 () {
-    oåoåoå()
+    oåoåoå_finetå(R1)
     led_e_blinke()
-    servo()
-    tibake_og_fram()
-    dancmove_1()
-    dancmove_3()
-    dancmove_4()
+    basic.pause(BPM * 2)
+    basic.clearScreen()
+    alle_smilefes("lei")
+    basic.pause(BPM * 16)
+    alle_smilefes("glad")
+    startlys = true
 }
-function ansikt_o () {
-    led.plot(1, 1)
-    led.plot(3, 1)
-    led.plot(3, 0)
-    led.plot(2, 4)
-    led.plot(0, 1)
-    led.plot(1, 0)
-    led.plot(0, 0)
-    led.plot(4, 1)
-    led.plot(4, 0)
-    led.plot(2, 2)
-    led.plot(1, 3)
-    led.plot(3, 3)
+function følge_linjen () {
+    while (bitbot.readLine(BBLineSensor.Left) == 1 && bitbot.readLine(BBLineSensor.Right) == 1) {
+        bitbot.go(BBDirection.Forward, 15)
+    }
+    while (bitbot.readLine(BBLineSensor.Right) == 1) {
+        bitbot.go(BBDirection.Forward, 15)
+    }
+    while (bitbot.readLine(BBLineSensor.Right) == 0) {
+        bitbot.rotate(BBRobotDirection.Right, 15)
+    }
+    while (true) {
+        if (bitbot.readLine(BBLineSensor.Left) == 0 && bitbot.readLine(BBLineSensor.Right) == 1) {
+            bitbot.rotate(BBRobotDirection.Left, 15)
+        } else if (bitbot.readLine(BBLineSensor.Left) == 1 && bitbot.readLine(BBLineSensor.Right) == 0) {
+            bitbot.rotate(BBRobotDirection.Right, 15)
+        } else if (bitbot.readLine(BBLineSensor.Left) == 1 && bitbot.readLine(BBLineSensor.Right) == 1) {
+            bitbot.go(BBDirection.Forward, 20)
+        } else if (bitbot.readLine(BBLineSensor.Left) == 0 && bitbot.readLine(BBLineSensor.Right) == 0) {
+            bitbot.stop(BBStopMode.Coast)
+            break;
+        }
+    }
 }
 input.onButtonPressed(Button.B, function () {
-    føl_linjen()
+    følge_linjen()
 })
 function r2 () {
-    oåoåoå()
+    oåoåoå_finetå(R2)
     led_e_blinke()
-    servo()
-    tibake_og_fram()
-    dancmove_1()
-    dancmove_3()
-    dancmove_4()
+    basic.pause(BPM * 2)
+    basic.clearScreen()
+    alle_smilefes("lei")
+    basic.pause(BPM * 8)
+    alle_smilefes("glad")
+    startlys = true
+    while (bitbot.readLine(BBLineSensor.Left) == 0 || bitbot.readLine(BBLineSensor.Right) == 0) {
+        bitbot.go(BBDirection.Forward, 20)
+    }
+}
+function blinke_blinke () {
+	
 }
 function dancmove_1 () {
     bitbot.goms(BBDirection.Forward, 1, 1)
@@ -262,27 +355,8 @@ function dancmove_1 () {
     bitbot.goms(BBDirection.Forward, 75, BPM)
     vent()
 }
-function dancmove_3 () {
-    for (let index = 0; index < 4; index++) {
-        bitbot.rotatems(BBRobotDirection.Left, 25, BPM)
-        basic.pause(BPM)
-        bitbot.goms(BBDirection.Forward, 40, BPM)
-        basic.pause(BPM)
-        bitbot.rotatems(BBRobotDirection.Right, 25, BPM)
-        basic.pause(BPM)
-        bitbot.goms(BBDirection.Forward, 40, BPM)
-        basic.pause(BPM)
-    }
-    for (let index = 0; index < 4; index++) {
-        bitbot.rotatems(BBRobotDirection.Left, 25, BPM)
-        basic.pause(BPM)
-        bitbot.goms(BBDirection.Reverse, 40, BPM)
-        basic.pause(BPM)
-        bitbot.rotatems(BBRobotDirection.Right, 25, BPM)
-        basic.pause(BPM)
-        bitbot.goms(BBDirection.Reverse, 40, BPM)
-        basic.pause(BPM)
-    }
+function r6 () {
+	
 }
 function _3 () {
     bitbot.setPixelColor(0, 0x00FFC0)
@@ -327,13 +401,38 @@ function dancmove_4 () {
     }
 }
 function r3 () {
-    oåoåoå()
+    oåoåoå_finetå(R3)
     led_e_blinke()
-    servo()
-    tibake_og_fram()
-    dancmove_1()
-    dancmove_3()
-    dancmove_4()
+    basic.pause(BPM * 2)
+    basic.clearScreen()
+    alle_smilefes("lei")
+    basic.pause(BPM * 8)
+    alle_smilefes("glad")
+    startlys = true
+    while (bitbot.readLine(BBLineSensor.Left) == 0 || bitbot.readLine(BBLineSensor.Right) == 0) {
+        bitbot.go(BBDirection.Forward, 20)
+    }
+}
+function start_oåoåoå2 () {
+    basic.pause(BPM * 1.5)
+    for (let index = 0; index < 2; index++) {
+        for (let index = 0; index < 3; index++) {
+            pins.servoWritePin(AnalogPin.P2, 180)
+            pins.servoWritePin(AnalogPin.P1, 0)
+            basic.pause(BPM / 2)
+            basic.clearScreen()
+            basic.pause(1)
+            pins.servoWritePin(AnalogPin.P2, 90)
+            pins.servoWritePin(AnalogPin.P1, 90)
+            basic.pause(BPM / 2)
+            basic.clearScreen()
+            basic.pause(1)
+        }
+        basic.pause(BPM)
+        basic.clearScreen()
+    }
+    vent()
+    basic.pause(BPM * 1.5)
 }
 function led_1 () {
     led.plot(1, 1)
@@ -346,6 +445,8 @@ function led_1 () {
     led.plot(3, 4)
 }
 let en_mer = 0
+let startlys = false
+let BPM2 = 0
 let R5 = 0
 let R4 = 0
 let R3 = 0
@@ -355,6 +456,7 @@ let starta = false
 let takt = 0
 let BPM = 0
 let klokke = 0
+let oåoåoå_bakrund = 0
 radio.setGroup(1)
 bitbot.select_model(BBModel.XL)
 pins.servoWritePin(AnalogPin.P2, 90)
@@ -367,6 +469,8 @@ R2 = -1950028692
 R3 = 1605682820
 R4 = 882193206
 R5 = 2001938284
+BPM2 = BPM / 2
+startlys = false
 // R ytme
 basic.forever(function () {
     if (starta == true) {
@@ -375,10 +479,10 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (starta == true) {
+    if (startlys == true) {
         while (true) {
             bitbot.ledRainbow()
-            basic.pause(BPM)
+            basic.pause(BPM * 2)
             bitbot.ledClear()
             for (let index = 0; index < 2; index++) {
                 _1()
